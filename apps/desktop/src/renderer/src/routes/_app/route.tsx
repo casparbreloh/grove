09687a04e732890306944983db6a416804a8a6ca
casparbreloh <caspar.breloh@gmail.com>
2026-08-15 00:37:04 +0200

    Build desktop chat scaffold

--- apps/desktop/src/renderer/src/routes/_app/route.tsx
+++ apps/desktop/src/renderer/src/routes/_app/route.tsx
@@ -10,11 +10,11 @@ export const Route = createFileRoute("/_app")({ component: AppLayout });
 
 function AppLayout() {
   return (
-    <SidebarProvider className="flex-col overflow-hidden bg-sidebar">
+    <SidebarProvider className="h-svh flex-col overflow-hidden bg-sidebar">
       <DesktopHeader />
       <div className="flex min-h-0 flex-1">
         <AppSidebar />
-        <SidebarInset className="min-w-0">
+        <SidebarInset className="min-h-0 min-w-0 overflow-hidden">
           <div className="flex min-h-0 flex-1 flex-col">
             <Outlet />
           </div>
